2a5a8871046146e17285abed33c2bb01bc95d118
diegojvd <diegov070501@gmail.com>
2024-12-03 20:03:44 -0500

    agregando datos reales en dashboard 1/2

--- lib/src/pages/dashboard.dart
+++ lib/src/pages/dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:credit_system/src/auth/auth_service.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
@@ -9,10 +10,36 @@ class DashboardPage extends StatefulWidget {
 }
 
 class _DashboardPageState extends State<DashboardPage> {
+  final _supabase = Supabase.instance.client;
   int overdueClients = 5;
   int totalClients = 100;
   int activeCredits = 50;
   double collectedLast30Days = 145658.58;
+
+  Future<int> getTotalClientes() async {
+    try {
+      final response = await _supabase.from('Clientes').select();
+      print(response);
+
+      final totalClientes = response?.length ?? 0;
+      return totalClientes;
+    } catch (error) {
+      return 0; 
+    }
+  }
+
+  Future<int> getTotalPrestamos() async {
+    try {
+      final response = await _supabase.from('Prestamos').select();
+      print(response);
+
+      final totalPrestamos = response?.length ?? 0;
+      return totalPrestamos;
+    } catch (error) {
+      print('Error obteniendo el total de préstamos: $error');
+      return 0; 
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,12 +73,13 @@ class _DashboardPageState extends State<DashboardPage> {
             onPressed: () {
               ScaffoldMessenger.of(context).showSnackBar(
                 SnackBar(
-                    content:
-                        Text('Clientes con cuentas vencidas: $overdueClients')),
+                  content:
+                      Text('Clientes con cuentas vencidas: $overdueClients'),
+                ),
               );
             },
           ),
-          // Menú
+
           PopupMenuButton<String>(
             onSelected: (value) {
               switch (value) {
@@ -76,28 +104,14 @@ class _DashboardPageState extends State<DashboardPage> {
               }
             },
             itemBuilder: (context) => [
+              const PopupMenuItem(value: 'Clientes', child: Text('Clientes')),
+              const PopupMenuItem(value: 'Préstamos', child: Text('Préstamos')),
+              const PopupMenuItem(value: 'Pagos', child: Text('Pagos')),
+              const PopupMenuItem(value: 'Consultas', child: Text('Consultas')),
               const PopupMenuItem(
-                value: 'Clientes',
-                child: Text('Clientes'),
-              ),
-              const PopupMenuItem(
-                value: 'Préstamos',
-                child: Text('Préstamos'),
-              ),
-              const PopupMenuItem(
-                value: 'Pagos',
-                child: Text('Pagos'),
-              ),
-              const PopupMenuItem(
-                value: 'Consultas',
-                child: Text('Consultas'),
-              ),
-              const PopupMenuItem(
-                value: 'Cerrar Sesión',
-                child: Text('Cerrar Sesión'),
-              ),
+                  value: 'Cerrar Sesión', child: Text('Cerrar Sesión')),
             ],
-          )
+          ),
         ],
       ),
       body: Padding(
@@ -109,37 +123,88 @@ class _DashboardPageState extends State<DashboardPage> {
               'Resumen',
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
-            const SizedBox(
-              height: 16,
-            ),
+            const SizedBox(height: 16),
             Expanded(
-                child: GridView.count(
-              crossAxisCount: 2,
-              crossAxisSpacing: 16,
-              mainAxisSpacing: 16,
-              children: [
-                _buildSummaryCard(
-                  title: 'Clientes',
-                  value: '$totalClients',
-                  icon: Icons.people,
-                ),
-                _buildSummaryCard(
-                  title: 'Préstamos Activos',
-                  value: '$activeCredits',
-                  icon: Icons.attach_money,
-                ),
-                _buildSummaryCard(
-                  title: 'Ultimos 30 días',
-                  value: '\$${collectedLast30Days.toStringAsFixed(2)}',
-                  icon: Icons.money,
-                ),
-                _buildSummaryCard(
-                  title: 'Cuentas vencidas',
-                  value: '$overdueClients',
-                  icon: Icons.warning,
-                ),
-              ],
-            )),
+              child: GridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                children: [
+                  FutureBuilder<int>(
+                    future:
+                        getTotalClientes(), 
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return _buildSummaryCard(
+                          title: 'Clientes',
+                          value: 'Cargando...',
+                          icon: Icons.people,
+                        );
+                      } else if (snapshot.hasError) {
+                        return _buildSummaryCard(
+                          title: 'Clientes',
+                          value: 'Error',
+                          icon: Icons.people,
+                        );
+                      } else if (snapshot.hasData) {
+                        return _buildSummaryCard(
+                          title: 'Clientes',
+                          value: '${snapshot.data}',
+                          icon: Icons.people,
+                        );
+                      } else {
+                        return _buildSummaryCard(
+                          title: 'Clientes',
+                          value: 'No disponible',
+                          icon: Icons.people,
+                        );
+                      }
+                    },
+                  ),
+                  FutureBuilder<int>(
+                    future:
+                        getTotalPrestamos(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return _buildSummaryCard(
+                          title: 'Préstamos Activos',
+                          value: 'Cargando...',
+                          icon: Icons.attach_money,
+                        );
+                      } else if (snapshot.hasError) {
+                        return _buildSummaryCard(
+                          title: 'Préstamos Activos',
+                          value: 'Error',
+                          icon: Icons.attach_money,
+                        );
+                      } else if (snapshot.hasData) {
+                        return _buildSummaryCard(
+                          title: 'Préstamos Activos',
+                          value: '${snapshot.data}',
+                          icon: Icons.attach_money,
+                        );
+                      } else {
+                        return _buildSummaryCard(
+                          title: 'Préstamos Activos',
+                          value: 'No disponible',
+                          icon: Icons.attach_money,
+                        );
+                      }
+                    },
+                  ),
+                  _buildSummaryCard(
+                    title: 'Ultimos 30 días',
+                    value: '\$${collectedLast30Days.toStringAsFixed(2)}',
+                    icon: Icons.money,
+                  ),
+                  _buildSummaryCard(
+                    title: 'Cuentas vencidas',
+                    value: '$overdueClients',
+                    icon: Icons.warning,
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
